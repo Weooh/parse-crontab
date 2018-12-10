f260121cd5fd054cba5de980508a34a3f897a3e4
weooh <shinzezhen@gmail.com>
2018-12-10 17:17:16 +0800

    Extend from older company project

--- crontab/_crontab.py
+++ crontab/_crontab.py
@@ -3,19 +3,19 @@
 crontab.py
 
 Written July 15, 2011 by Josiah Carlson
-Released under the GNU LGPL v2.1 and v3
-available:
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
-http://www.gnu.org/licenses/lgpl.html
+Modify in Dec 10 2018 by Weooh
+
+Released under the GNU LGPL v2.1
+available: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 
 Other licenses may be available upon request.
 
 '''
 
 from collections import namedtuple
-from datetime import datetime, timedelta
+import datetime
 import sys
-import warnings
+from pytz import timezone
 
 _ranges = [
     (0, 59),
@@ -36,7 +36,7 @@ _attribute = [
 _alternate = {
     3: {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,
         'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov':11, 'dec':12},
-    4: {'sun': 0, 'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5, 'sat': 6},
+    4: {'sun': 6, 'mon': 0, 'tue': 1, 'wed': 2, 'thu': 3, 'fri': 4, 'sat': 5},
 }
 _aliases = {
     '@yearly':  '0 0 1 1 *',
@@ -47,31 +47,18 @@ _aliases = {
     '@hourly':  '0 * * * *',
 }
 
-WARNING_CHANGE_MESSAGE = '''\
-Version 0.22.0+ of crontab will use datetime.utcnow() and
-datetime.utcfromtimestamp() instead of datetime.now() and
-datetime.fromtimestamp() as was previous. This had been a bug, which will be
-remedied. If you would like to keep the *old* behavior:
-`ct.next(..., default_utc=False)` . If you want to use the new behavior *now*:
-`ct.next(..., default_utc=True)`. If you pass a datetime object with a tzinfo
-attribute that is not None, timezones will *just work* to the best of their
-ability. There are tests...'''
-
-
 if sys.version_info >= (3, 0):
     _number_types = (int, float)
     xrange = range
 else:
     _number_types = (int, long, float)
 
-MINUTE = timedelta(minutes=1)
-HOUR = timedelta(hours=1)
-DAY = timedelta(days=1)
-WEEK = timedelta(days=7)
-MONTH = timedelta(days=28)
-YEAR = timedelta(days=365)
-
-WARN_CHANGE = object()
+MINUTE = datetime.timedelta(minutes=1)
+HOUR = datetime.timedelta(hours=1)
+DAY = datetime.timedelta(days=1)
+WEEK = datetime.timedelta(days=7)
+MONTH = datetime.timedelta(days=28)
+YEAR = datetime.timedelta(days=365)
 
 # find the next scheduled time
 def _end_of_month(dt):
@@ -155,7 +142,7 @@ _decrements = [
     lambda dt,x: dt.replace(minute=59),
     lambda dt,x: dt.replace(hour=23),
     _day_decr_reset,
-    lambda dt,x: dt.replace(month=12) if x < -DAY else dt,
+    lambda dt,x: dt.replace(month=12)if x < -DAY else dt,
     lambda dt,x: dt,
 ]
 
@@ -176,7 +163,6 @@ class _Matcher(object):
         self.allowed = set()
         self.end = None
         self.any = '*' in self.split or '?' in self.split
-
         for it in self.split:
             al, en = self._parse_crontab(which, it)
             if al is not None:
@@ -185,45 +171,23 @@ class _Matcher(object):
         _assert(self.end is not None,
             "improper item specification: %r", entry.lower()
         )
-
     def __call__(self, v, dt):
-        for i, x in enumerate(self.split):
-            if x == 'l':
-                if v == _end_of_month(dt).day:
-                    return True
-
-            elif x.startswith('l'):
-                # We have to do this in here, otherwise we can end up, for
-                # example, accepting *any* Friday instead of the *last* Friday.
-                if dt.month == (dt + WEEK).month:
-                    continue
-
-                x = x[1:]
-                if x.isdigit():
-                    x = int(x) if x != '7' else 0
-                    if v == x:
-                        return True
-                    continue
-
-                start, end = map(int, x.partition('-')[::2])
-                allowed = set(range(start, end+1))
-                if 7 in allowed:
-                    allowed.add(0)
-                if v in allowed:
-                    return True
-
+        if 'l' in self.split:
+            if v == _end_of_month(dt).day:
+                return True
+        elif any(x.startswith('l') for x in self.split):
+            okay = dt.month != (dt + WEEK).month
+            if okay and (self.any or v in self.allowed):
+                return True
         return self.any or v in self.allowed
-
     def __lt__(self, other):
         if self.any:
             return self.end < other
         return all(item < other for item in self.allowed)
-
     def __gt__(self, other):
         if self.any:
             return _ranges[self.which][0] > other
         return all(item > other for item in self.allowed)
-
     def _parse_crontab(self, which, entry):
         '''
         This parses a single crontab field and returns the data necessary for
@@ -231,7 +195,6 @@ class _Matcher(object):
 
         See the README for information about what is accepted.
         '''
-
         # this handles day of week/month abbreviations
         def _fix(it):
             if which in _alternate and not it.isdigit():
@@ -239,28 +202,31 @@ class _Matcher(object):
                     return _alternate[which][it]
             _assert(it.isdigit(),
                 "invalid range specifier: %r (%r)", it, entry)
-            it = int(it, 10)
-            _assert(_start <= it <= _end_limit,
-                "item value %r out of range [%r, %r]",
-                it, _start, _end_limit)
-            return it
+            return int(it, 10)
 
         # this handles individual items/ranges
         def _parse_piece(it):
             if '-' in it:
                 start, end = map(_fix, it.split('-'))
-                # Allow "sat-sun"
-                if which == 4 and end == 0:
-                    end = 7
             elif it == '*':
                 start = _start
                 end = _end
             else:
                 start = _fix(it)
                 end = _end
+                
+                _assert(_start <= start <= _end_limit,
+                "range start value %r out of range [%r, %r]",
+                    start, _start, _end_limit)
+                _assert(_start <= end <= _end_limit,
+                    "range end value %r out of range [%r, %r]",
+                    end, _start, _end_limit)
+                _assert(start <= end,
+                    "range start value %r > end value %r", start, end)
+                
                 if increment is None:
                     return set([start])
-
+                
             _assert(_start <= start <= _end_limit,
                 "range start value %r out of range [%r, %r]",
                 start, _start, _end_limit)
@@ -269,6 +235,7 @@ class _Matcher(object):
                 end, _start, _end_limit)
             _assert(start <= end,
                 "range start value %r > end value %r", start, end)
+            
             return set(range(start, end+1, increment or 1))
 
         _start, _end = _ranges[which]
@@ -286,15 +253,11 @@ class _Matcher(object):
                 "you can only specify a bare 'L' in the 'day' field")
             return None, _end
 
-        # for the last 'friday' of the month, for example
+        # last day of the week
         elif entry.startswith('l'):
             _assert(which == 4,
                 "you can only specify a leading 'L' in the 'weekday' field")
-            es, _, ee = entry[1:].partition('-')
-            _assert((entry[1:].isdigit() and 0 <= int(es) <= 7) or
-                    (_ and es.isdigit() and ee.isdigit() and 0 <= int(es) <= 7 and 0 <= int(ee) <= 7),
-                "last <day> specifier must include a day number or range in the 'weekday' field, you entered %r", entry)
-            return None, _end
+            entry = entry.lstrip('l')
 
         increment = None
         # increments
@@ -306,11 +269,13 @@ class _Matcher(object):
                 increment)
 
         # allow Sunday to be specified as weekday 7
-        if which == 4:
-            _end_limit = 7
+        #if which == 4:
+            #_end_limit = 7
 
-        # handle singles and ranges
-        good = _parse_piece(entry)
+        # handle all of the a,b,c and x-y,a,b entries
+        good = set()
+        for it in entry.split(','):
+            good.update(_parse_piece(it))
 
         # change Sunday to weekday 0
         if which == 4 and 7 in good:
@@ -319,11 +284,11 @@ class _Matcher(object):
 
         return good, _end
 
-
 class CronTab(object):
-    __slots__ = 'matchers',
-    def __init__(self, crontab):
+    __slots__ = ('matchers', 'timezone')
+    def __init__(self, crontab, tz=None):
         self.matchers = self._make_matchers(crontab)
+        self.timezone = tz
 
     def _make_matchers(self, crontab):
         '''
@@ -332,13 +297,18 @@ class CronTab(object):
         crontab = _aliases.get(crontab, crontab)
         matchers = [_Matcher(which, entry)
                         for which, entry in enumerate(crontab.split())]
-
         if len(matchers) == 5:
             matchers.append(_Matcher(5, '*'))
         _assert(len(matchers) == 6,
             "improper number of cron entries specified")
-
-        return Matcher(*matchers)
+        matchers = Matcher(*matchers)
+        if not matchers.day.any:
+            _assert(matchers.weekday.any,
+                "missing a wildcard specifier for weekday")
+        if not matchers.weekday.any:
+            _assert(matchers.day.any,
+                "missing a wildcard specifier for day")
+        return matchers
 
     def _test_match(self, index, dt):
         '''
@@ -348,32 +318,22 @@ class CronTab(object):
         at = _attribute[index]
         attr = getattr(dt, at)
         if at == 'isoweekday':
-            attr = attr() % 7
+            attr = attr() - 1
         return self.matchers[index](attr, dt)
 
-    def next(self, now=None, increments=_increments, delta=True, default_utc=WARN_CHANGE):
+    def next(self, now=None, increments=_increments, delta=True):
         '''
         How long to wait in seconds before this crontab entry can next be
         executed.
         '''
-        if default_utc is WARN_CHANGE and (isinstance(now, _number_types) or (now and not now.tzinfo)):
-            warnings.warn(WARNING_CHANGE_MESSAGE, FutureWarning, 2)
-            default_utc = False
-
-        now = now or (datetime.utcnow() if default_utc else datetime.now())
+        now = now or datetime.datetime.now()
         if isinstance(now, _number_types):
-            now = datetime.utcfromtimestamp(now) if default_utc else datetime.fromtimestamp(now)
-
-        # handle timezones if the datetime object has a timezone and get a
-        # reasonable future/past start time
-        onow, now = now, now.replace(tzinfo=None)
-        tz = onow.tzinfo
+            now = datetime.datetime.fromtimestamp(now, self.timezone)
+        # get a reasonable future/past start time
         future = now.replace(second=0, microsecond=0) + increments[0]()
         if future < now:
             # we are going backwards...
             _test = lambda: future.year < self.matchers.year
-            if now.second or now.microsecond:
-                future = now.replace(second=0, microsecond=0)
         else:
             # we are going forwards
             _test = lambda: self.matchers.year < future.year
@@ -404,23 +364,16 @@ class CronTab(object):
             "crontab: %r\n" \
             "now: %r", ' '.join(m.input for m in self.matchers), now)
         delay = future - now
-        if tz:
-            delay += tz.utcoffset(now)
-            delay -= tz.utcoffset(future)
-
         if not delta:
-            begin = datetime(1970, 1, 1)
-            delay = future - begin
-            if tz:
-                delay -= tz.utcoffset(future)
+            delay = future - datetime.datetime(1970, 1, 1)
         return delay.days * 86400 + delay.seconds + delay.microseconds / 1000000.
 
-    def previous(self, now=None, delta=True, default_utc=WARN_CHANGE):
-        return self.next(now, _decrements, delta, default_utc)
+    def previous(self, now=None, delta=True):
+        return self.next(now, _decrements, delta)
 
     def test(self, entry):
         if isinstance(entry, _number_types):
-            entry = datetime.utcfromtimestamp(entry)
+            entry = datetime.datetime.utcfromtimestamp(entry)
         for index in xrange(6):
             if not self._test_match(index, entry):
                 return False
